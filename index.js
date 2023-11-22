b05675eecbd3786807161de5e760a2db8ad7610d
Archana-Lokini <150821236+Archana-Lokini@users.noreply.github.com>
2023-11-22 12:06:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const monthEl=document.querySelector(".date h1");
+const fullDateEl=document.querySelector(".date p");
+const daysEl=document.querySelector(".days");
+const monthInx=new Date().getMonth();
+const lastDay=new Date(new Date().getFullYear(),monthInx +1 ,0).getDate();
+const firstDay=new Date(new Date().getFullYear(),monthInx, 1).getDay()-1;
+console.log(firstDay);
+
+
+const months=[
+    "jun",
+    "feb",
+    "march",
+    "aprill",
+    "may",
+    "june",
+    "july",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec",
+];
+monthEl.innerText=months[monthInx];
+fullDateEl.innerText=new Date().toDateString();
+let days="";
+for(let i=firstDay;i>0;i--){
+    days+=`<div class="empty"></div>`;
+
+}
+for(let i=1;i<=lastDay;i++){
+    if(i===new Date().getDate()){
+        days+=`<div class="today">${i}</div>`;
+    }else{
+days+=`<div>${i}</div>`;
+}
+}
+daysEl.innerHTML=days;
